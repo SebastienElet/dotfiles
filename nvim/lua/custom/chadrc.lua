3ddd817a20dbb7017884965bfd3ac7fedba13c37
Sébastien Elet <sebastien@elet.fr>
2022-09-29 11:57:36 +0200

    refactor(nvim): move mappings in a dedicated folder
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,23 +1,6 @@
 local M = {}
 M.ui = {}
 M.plugins = require("custom.plugins")
-
-M.mappings = {
-	usage = {
-		n = {
-			-- Bad idea, this is conflicting with quickfix
-			-- ["<CR>"] = { ":nohlsearch<CR>:w<CR>", "Save file", {} },
-			["<Tab>"] = { "%", "Find the next item", {} },
-			-- cargo install typos-cli
-			["<Leader>t"] = { ':execute "!typos" @%<CR>', "Run typos cli on the current file", {} },
-		},
-		v = {
-			["<Tab>"] = { ">gv", "Indent the selected block", {} },
-			["<S-Tab>"] = { "<gv", "Unindent the selected block", {} },
-			[">"] = { ">gv", "Indent the selected block", {} },
-			["<"] = { "<gv", "Unindent the selected block", {} },
-		},
-	},
-}
+M.mappings = require("custom.mappings")
 
 return M
