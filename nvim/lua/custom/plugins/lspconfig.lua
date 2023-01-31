8177568fbeeb8e09ca25cfdcf0361cba6d28da94
Sébastien Elet <sebastien@elet.fr>
2023-01-31 08:57:25 +0100

    refactor(lsp): use 8GB of ram for typescript
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -1,12 +1,20 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
-local servers = { "html", "cssls", "tsserver", "rust_analyzer", "graphql", "jsonls" }
+local servers = { "html", "cssls", "rust_analyzer", "graphql", "jsonls" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
+
+lspconfig.tsserver.setup({
+  settings = {
+    maxTsServerMemory = 8192,
+  },
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
