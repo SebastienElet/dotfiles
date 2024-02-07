dc58857badc16a3f6d984687a088f19c765d2f6a
Sébastien Elet <sebastien@elet.fr>
2024-02-07 12:55:34 +0100

    refactor(wezterm): alternate between nord and nord light
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,25 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "nord"
+	else
+		return "nord-light"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
 
 return {
-  font = wezterm.font 'Iosevka Nerd Font',
-  color_scheme = 'nord',
-  hide_tab_bar_if_only_one_tab = true,
+	font = wezterm.font("Iosevka Nerd Font"),
+	color_scheme = "nord",
+	hide_tab_bar_if_only_one_tab = true,
 }
