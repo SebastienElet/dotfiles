41fddbccf92b3ad7528cae5e67b222aca7ee731d
Sébastien Elet <sebastien@elet.fr>
2024-02-14 08:55:44 +0100

    feat(nvim): add bash formatting
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,10 +5,14 @@ return {
     opts = {
       formatters_by_ft = {
         sql = { "sqlfluff" },
+        sh = { "shfmt" },
       },
       formatters = {
         sqlfluff = {
           args = { "fix", "--force", "-" },
+        },
+        shfmt = {
+          prepend_args = { "-i", "2", "-ci" },
         },
       },
     },
