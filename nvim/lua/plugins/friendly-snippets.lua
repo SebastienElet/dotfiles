c3e369e2125af92acda16fdc2295c7b4b4ebc0c5
Sébastien Elet <sebastien@elet.fr>
2024-06-10 16:53:15 +0200

    chore: upgrade lazy vim plugins
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- nvim/lua/plugins/friendly-snippets.lua
+++ nvim/lua/plugins/friendly-snippets.lua
@@ -1,7 +1,8 @@
 return {
   "rafamadriz/friendly-snippets",
   config = function()
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
+    -- TODO: this does not work anymore
+    -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
     -- TODO: load from .vscode folders
     -- require("luasnip.loaders.from_vscode").lazy_load()
   end,
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,13 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-
     ---@type lspconfig.options
     servers = {
       tsserver = {
         init_options = {
           maxTsServerMemory = 16384,
         },
+      },
+      sqlls = {
+        cmd = { "sql-language-server", "up", "--method", "stdio" },
       },
     },
   },
