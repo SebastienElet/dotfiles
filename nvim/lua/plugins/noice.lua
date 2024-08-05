294212843b36c1c739a58163212357a89c4b1239
Sébastien Elet <sebastien@elet.fr>
2024-08-05 10:38:52 +0200

    refactor: mute ts errors
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  -- REMOVE THIS once this issue is fixed: https://github.com/yioneko/vtsls/issues/159
+  opts = {
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "Request textDocument/inlayHint failed",
+        },
+        opts = { skip = true },
+      },
+    },
+  },
+}
