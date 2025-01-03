c72ab218416164cb0b363640c21c3edd38e6ca84
Sébastien Elet <sebastien@elet.fr>
2025-01-03 14:42:43 +0100

    refactor(conform): increase default timeout
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,6 +3,9 @@ return {
     "stevearc/conform.nvim",
     dependencies = { "mason.nvim" },
     opts = {
+      default_format_opts = {
+        timeout_ms = 5000,
+      },
       formatters = {
         sqlfluff = {
           args = { "fix", "--dialect=postgres", "-" },
