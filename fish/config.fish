2fd5580b07d0b8d6b290343df11fead9fcd54654
Sébastien Elet <sebastien@elet.fr>
2025-01-10 17:29:49 +0100

    feat(fish): enable starfish
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Enable vi bindings
 set -g fish_key_bindings fish_vi_key_bindings
+
+# Ensure Starship is installed
+if test -n (type -q starship)
+    # Enable Starship
+    starship init fish | source
+else
+    echo "Starship is not installed. Please install it from https://starship.rs/"
+end
