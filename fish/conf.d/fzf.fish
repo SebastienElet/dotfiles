fb1b2482425c4465e439a901767e7180d02a84a9
Sébastien Elet <sebastien@elet.fr>
2025-01-27 10:36:49 +0100

    refactor(fish): update fish install
    Signed-off-by: Sébastien Elet <sebastien@elet.fr>

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,8 +1,3 @@
-if test -d ~/.fzf/bin
-    set -x PATH ~/.fzf/bin $PATH
-    fzf --fish | source
-end
-
 # fzf.fish is only meant to be used in interactive mode. If not in interactive mode and not in CI, skip the config to speed up shell startup
 if not status is-interactive && test "$CI" != true
     exit
