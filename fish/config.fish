88ff826ef699f15ba75fb953235c9f9a1ead3df4
Sébastien Elet <sebastien@elet.fr>
2025-12-24 18:20:47 +0100

    chore(fish): add orbstack shell integration

--- fish/config.fish
+++ fish/config.fish
@@ -18,3 +18,7 @@ end
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
