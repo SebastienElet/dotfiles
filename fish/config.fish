0dc3931a7dad2df07770d4baca7247a94ba89fcd
Sébastien Elet <sebastien@elet.fr>
2025-12-24 18:21:33 +0100

    refactor(fish): make orbstack integration conditional

--- fish/config.fish
+++ fish/config.fish
@@ -21,4 +21,6 @@ set --export PATH $BUN_INSTALL/bin $PATH
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+if test -f ~/.orbstack/shell/init2.fish
+    source ~/.orbstack/shell/init2.fish
+end
