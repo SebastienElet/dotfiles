afc71a635b7aa5fea8d35999dcacc01f1163b559
Sébastien Elet <sebastien@elet.fr>
2026-01-11 08:14:03 +0100

    fix(fish): correct type -q syntax in conditional aliases
    `type -q` returns an exit code, not a string, so `test -n` was incorrect.

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -13,20 +13,20 @@ alias t='tmux'
 alias tm='tmux'
 alias upgrade='~/.dotfiles/scripts/upgrade'
 
-if test -n (type -q nvim)
+if type -q nvim
     alias vim='nvim'
     alias v='nvim'
     alias n='nvim'
 end
 
-if test -n (type -q cursor)
+if type -q cursor
     alias c='cursor'
 end
 
-if test -n (type -q tokei)
+if type -q tokei
     alias loc='tokei'
 end
 
-if test -n (type -q eza)
+if type -q eza
     alias ls='eza'
 end
