d7b14c92e55e168bad2135c518ff40c3bbffe6f2
Sébastien Elet <sebastien@elet.fr>
2026-01-12 08:38:36 +0100

    feat(nvim): Switch to GitHub theme with auto dark mode detection

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,41 @@
 return {
   {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = false,
+          dim_inactive = false,
+          styles = {
+            comments = "italic",
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "f-person/auto-dark-mode.nvim",
     opts = {
-      flavour = "auto",
-      transparent_background = true,
-      dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-      },
+      update_interval = 1000, -- Check every second
+      set_dark_mode = function()
+        vim.o.background = "dark"
+        vim.cmd("colorscheme github_dark")
+      end,
+      set_light_mode = function()
+        vim.o.background = "light"
+        vim.cmd("colorscheme github_light")
+      end,
     },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = function() end, -- Handled by auto-dark-mode
     },
   },
 }
