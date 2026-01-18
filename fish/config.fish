81fbba209a2ef96af8aca6ba42eb5a8e8697b231
Sébastien Elet <sebastien@elet.fr>
2026-01-18 17:47:15 +0100

    chore(fish): use HOME for pnpm path

--- fish/config.fish
+++ fish/config.fish
@@ -31,7 +31,7 @@ if test -f ~/.orbstack/shell/init2.fish
 end
 
 # pnpm
-set -gx PNPM_HOME /Users/sebastien/Library/pnpm
+set -gx PNPM_HOME $HOME/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
