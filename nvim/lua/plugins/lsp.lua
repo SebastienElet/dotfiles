62c5688c1c4d833d8e1b232f2f410d883a85d2cb
Sébastien Elet <sebastien@elet.fr>
2026-05-18 09:41:49 +0200

    fix(nvim): run eslint fixes on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,6 +4,24 @@ return {
 
     ---@type lspconfig.options
     servers = {
+      eslint = {
+        on_attach = function(client, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              client:request_sync("workspace/executeCommand", {
+                command = "eslint.applyAllFixes",
+                arguments = {
+                  {
+                    uri = vim.uri_from_bufnr(bufnr),
+                    version = vim.lsp.util.buf_versions[bufnr],
+                  },
+                },
+              }, nil, bufnr)
+            end,
+          })
+        end,
+      },
       vtsls = {
         settings = {
           typescript = {
